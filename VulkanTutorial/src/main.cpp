a6abd75561068cbaf618e43dd2305a90db40879b
meta <troctsch.cpp@gmail.com>
2023-04-24 16:15:28 +0200

    VulkanTutorial
    - refined clang tidy config
    - fixed cmake's normalization path script
    - added minimal code to build sdl2 program

--- VulkanTutorial/src/main.cpp
+++ VulkanTutorial/src/main.cpp
@@ -1,4 +1,31 @@
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
 auto main( int /*unused*/, char * /*unused*/[] ) -> int
 {
+  if( SDL_Init( SDL_INIT_VIDEO ) != 0 )
+    return 1;
+
+  constexpr unsigned int width = 800, height = 600;
+  auto *window = SDL_CreateWindow( "VulkanTutorial", SDL_WINDOWPOS_UNDEFINED,
+                                   SDL_WINDOWPOS_UNDEFINED, width, height,
+                                   SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN );
+  if( window == nullptr )
+    return 1;
+
+  bool done = false;
+  while( !done )
+  {
+    SDL_Event event;
+    while( SDL_PollEvent( &event ) != 0 )
+    {
+      if( event.type == SDL_QUIT )
+        done = true;
+    }
+  }
+
+  SDL_DestroyWindow( window );
+  SDL_Quit();
+
   return 0;
 }
