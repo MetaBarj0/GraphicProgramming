8b910f2348dba210b6a442dc9390fdb1d298b480
meta <troctsch.cpp@gmail.com>
2023-04-24 14:21:10 +0200

    VulkanTutorial
    - Simplified a cmake script

--- VulkanTutorial/cmake/add_clang_compile_options.cmake
+++ VulkanTutorial/cmake/add_clang_compile_options.cmake
@@ -1,14 +1,22 @@
-if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-    add_compile_options(-Wall -Wextra -Wshadow -Wnon-virtual-dtor -pedantic
-      -Wold-style-cast -Wcast-align -Wunused -Woverloaded-virtual -Wconversion
-      -Wdouble-promotion -Wformat=2 -Wimplicit-fallthrough)
-
-    # unsupported on windows platforms
-    if(NOT WIN32)
-      add_compile_options(-fsanitize=safe-stack)
-    endif()
+if(NOT CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+  return()
 endif()
 
-if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-  add_compile_options(-fsanitize=undefined)
+add_compile_options(-Wall -Wextra -Wshadow -Wnon-virtual-dtor -pedantic
+  -Wold-style-cast -Wcast-align -Wunused -Woverloaded-virtual -Wconversion
+  -Wdouble-promotion -Wformat=2 -Wimplicit-fallthrough)
+
+if(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
+  return()
 endif()
+
+add_compile_options(-fsanitize=undefined)
+add_link_options(-fsanitize=undefined)
+
+# unsupported on windows platforms
+if(WIN32)
+  return()
+endif()
+
+add_compile_options(-fsanitize=safe-stack)
+add_link_options(-fsanitize=safe-stack)
