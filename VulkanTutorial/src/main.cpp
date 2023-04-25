52cf8789916617697b4a02e0b5c2b54fe5e9202d
meta <troctsch.cpp@gmail.com>
2023-04-25 15:58:27 +0200

    VulkanTutorial
    - embedded code in a class

--- VulkanTutorial/src/main.cpp
+++ VulkanTutorial/src/main.cpp
@@ -1,39 +1,91 @@
 #define SDL_MAIN_HANDLED
+
 #include <SDL2/SDL.h>
+#include <fmt/core.h>
+#include <iostream>
 #include <vulkan/vulkan.hpp>
 
-auto main( int /*unused*/, char * /*unused*/[] ) -> int
+class HelloTriangleApplication
 {
-  if( SDL_Init( SDL_INIT_VIDEO ) != 0 )
-    return 1;
-
-  constexpr unsigned int width = 800, height = 600;
-  auto *window = SDL_CreateWindow( "VulkanTutorial", SDL_WINDOWPOS_UNDEFINED,
-                                   SDL_WINDOWPOS_UNDEFINED, width, height,
-                                   SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN );
-
-  uint32_t extensionCount = 0;
-  vkEnumerateInstanceExtensionProperties( nullptr, &extensionCount, nullptr );
-
-  if( extensionCount == 0 )
-    return 1;
-
-  if( window == nullptr )
-    return 1;
-
-  bool done = false;
-  while( !done )
+public:
+  void run()
   {
-    SDL_Event event;
-    while( SDL_PollEvent( &event ) != 0 )
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+
+private:
+  SDL_Window *window_{ nullptr };
+
+  void initWindow()
+  {
+    if( SDL_Init( SDL_INIT_VIDEO ) == 0 )
+      throw std::runtime_error{ "Could not initialize SDL2 video subsystem." };
+
+    constexpr unsigned int width = 800, height = 600;
+    window_ = SDL_CreateWindow( "VulkanTutorial", SDL_WINDOWPOS_UNDEFINED,
+                                SDL_WINDOWPOS_UNDEFINED, width, height,
+                                SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN );
+
+    if( window_ == nullptr )
+      throw std::runtime_error{ "Could not create a SDL2 window" };
+  }
+
+  void initVulkan()
+  {
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties( nullptr, &extensionCount, nullptr );
+
+    if( extensionCount == 0 )
     {
-      if( event.type == SDL_QUIT )
-        done = true;
+      throw std::runtime_error{
+        "Could not get supported vulkan extension count."
+      };
     }
   }
 
-  SDL_DestroyWindow( window );
-  SDL_Quit();
+  void mainLoop()
+  {
+    bool done = false;
+    while( !done )
+    {
+      SDL_Event event;
+      while( SDL_PollEvent( &event ) != 0 )
+      {
+        if( event.type == SDL_QUIT )
+          done = true;
+      }
+    }
+  }
 
-  return 0;
+  void cleanup()
+  {
+    SDL_DestroyWindow( window_ );
+    SDL_Quit();
+  }
+};
+
+auto main( int /*unused*/, char * /*unused*/[] ) -> int
+{
+  HelloTriangleApplication app;
+
+  try
+  {
+    app.run();
+  }
+  catch( const std::exception &e )
+  {
+    fmt::print( "Global failure: {}\n", e.what() );
+    fmt::print( "Press any key to continue...\n" );
+    std::cin.get();
+
+    return EXIT_FAILURE;
+  }
+
+  fmt::print( "Press any key to continue...\n" );
+  std::cin.get();
+
+  return EXIT_SUCCESS;
 }
